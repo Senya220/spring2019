2359bf308e35b87800695dbb094ea60db7a57d3a
Senya <letgamemc@gmail.com>
2019-03-02 12:45:28 +0200

    Lucky Lottery

--- lottery.py
+++ lottery.py
@@ -6,28 +6,33 @@ win = [0, 0, 0, 1, 1]
 bal = 0
 
 print("♥💰Lucky Lottery💰♥")
+print("")
 
 while a == 1:
     t.sleep(1)
     print("Starting game...")
+    print("")
     t.sleep(2)
     won = r.choice(win)
     if won == 1:
         print("You win $15!")
         bal = bal + 15
-        print("Current balance: $", bal)
+
     if won == 0:
         print("You failed!")
-        print("Current balance: $", bal)
 
-    b = str(input("Want replay? ('yes' or 'no') "))
-    if b != 'no':
-        if bal < 10:
-            print("You haven`t enough money.")
-            a = 0
-        if bal >= 10:
-            bal = bal - 10
-            a = 1
+    print("Current balance: $", bal)
+    print("")
+
+    if bal < 10:
+        print("You haven`t enough money to replay.")
+        print("")
+        a = 0
+    if bal >= 10:
+        print("Payed $10 successfully. Be ready to next round! ")
+        print("")
+        bal = bal - 10
+        a = 1
 
 print("Game ended.")
 print("Goodbye!")
