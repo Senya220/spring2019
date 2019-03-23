4f8f6f30e295927717510fbc7dd7cb9d9bcff083
Senya <letgamemc@gmail.com>
2019-03-23 11:39:25 +0200

    Hacker Speak Generator 2.0 Changelog: Added Decrypt Some bug fixed Git now ignore Output.txt

---
+++ Hacker Speak Generator/Decrypt.py
@@ -0,0 +1,35 @@
+import time
+
+outpt = open('Output.txt', 'a')
+inpt = str(input("Input encrypted text here (If tou input numbers, they may be decrypted!!!): "))
+a = 0
+b = 0
+lst = []
+
+outpt.write(" [ ")
+
+for i in range(len(inpt)):
+    lst.append(inpt[a])
+    a = a + 1
+
+for i in range(len(lst)):
+    if lst[b] == '4':
+        outpt.write("a")
+    else:
+        if lst[b] == '3':
+            outpt.write("e")
+        else:
+            if lst[b] == '0':
+                outpt.write("o")
+            else:
+                if lst[b] == '1':
+                    outpt.write("i")
+                else:
+                    outpt.write(lst[b])
+    b = b+1
+    time.sleep(0.2)
+    print("Process is running...")
+
+outpt.write(" ]")
+outpt.close()
+print("Process finished")
