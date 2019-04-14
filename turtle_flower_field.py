d2aa068f85eb88d692a001fbe7221a05657970ac
Senya220 <letgamemc@gmail.com>
2019-04-14 17:42:54 +0300

    Turtle FlowerField 3.0 CIRCLES UPDATE Changelog: Flowers now ROUND!!! Some bug fixes Set background color to cyan. Sun is back!

--- turtle_flower_field.py
+++ turtle_flower_field.py
@@ -25,23 +25,51 @@ def draw_square(color, size_f):
     t.end_fill()
 
 
-def draw_flower(size, x, y, color, color_center):
-    print(str("Drawing " + color + " flower with " + color_center + " center."))
-    t.goto(x+size, y+size)
+def draw_circle(color, size_f):
+    t.begin_fill()
     t.pendown()
-    draw_square(color, size)
-    t.goto(x-size, y-size)
-    draw_square(color, size)
-    t.goto(x-size, y+size)
-    draw_square(color, size)
-    t.goto(x+size, y-size)
-    draw_square(color, size)
-    t.goto(x, y)
-    t.color(color)
-    draw_square(color_center, size)
-    t.shape("turtle")
-    t.hideturtle()
+    t.color(color, "black")
+    t.circle(size_f)
+    t.fillcolor(color)
+    t.end_fill()
+    t.pencolor("black")
+    t.circle(size_f)
     t.penup()
+
+
+
+def draw_flower(size, x, y, color, color_center, angle):
+    print(str("Drawing " + color + " flower with " + color_center + " center."))
+    t.goto(x, y)
+
+    t.setheading(angle)
+    t.forward(size * 2.5)
+    t.setheading(angle + 90)
+    draw_circle(color, size)
+    t.goto(x, y)
+
+    t.setheading(angle + 90)
+    t.forward(size * 2.5)
+    t.setheading(angle + 180)
+    draw_circle(color, size)
+    t.goto(x, y)
+
+    t.setheading(angle + 180)
+    t.forward(size * 2.5)
+    t.setheading(angle + 270)
+    draw_circle(color, size)
+    t.goto(x, y)
+
+    t.setheading(angle + 270)
+    t.forward(size * 2.5)
+    t.setheading(angle + 360)
+    draw_circle(color, size)
+    t.goto(x, y)
+
+    t.setheading(90)
+    t.forward(size * 1)
+    t.setheading(180)
+    draw_circle(color_center, size)
 
 
 def draw_sun():
@@ -61,10 +89,13 @@ def draw_sun():
     t.end_fill()
 
 
-colors = ["black", "cyan", "navy", "blue", "red", "yellow", "orange", "green"]
+colors = ["magenta", "cyan", "navy", "blue", "red", "yellow", "orange", "green"]
 
 # --------------------
 
+t.bgcolor("cyan")
+t.hideturtle()
+t.penup()
 t.speed(0)
 
 # draw_sun()
@@ -75,12 +106,14 @@ t.speed(0)
 # draw_flower(50, 0, -100, "pink", "navy")
 # draw_flower(50, 80, 145, "navy", "cyan")
 # draw_flower(50, 100, -150, "orange", "yellow")
-i = 10000000
+i = 50
+
+draw_sun()
 
 for g in range(i):
     i = i+1
-    draw_flower(randint(50, 100), randint(-300, 300), randint(-300, 300), choice(colors), choice(colors))
+    draw_flower(randint(10, 40), randint(-300, 300), randint(-300, 50), choice(colors), choice(colors), randint(0, 90))
     w.sleep(0.0001)
 
-# r.randint(-300, 300)
+print("Finished")
 t.exitonclick()
