8bb2b639ca90191a313dfbca564a85ef7d0cac9d
Senya220 <letgamemc@gmail.com>
2019-04-20 13:10:11 +0300

    Turtle FlowerField 4.0 FIELD UPDATE (FINAL) Changelog: Now turtle draws a full flower field! Added function "draw_stick()" Deleted cyan and black flower's color. Added "Examples" Cleared all useless comments

--- turtle_flower_field.py
+++ turtle_flower_field.py
@@ -35,9 +35,30 @@ def draw_circle(color, size_f):
     t.penup()
 
 
+def draw_stick(x, y):
+    t.goto(x, y - 400)
+    t.setheading(90)
+    t.pensize(20)
+    t.pencolor("green")
+    t.pendown()
+    t.goto(x, y)
+    t.pensize(3)
+    t.pencolor("black")
+    t.penup()
+    t.goto(x - 9, y - 400)
+    t.pendown()
+    t.goto(x - 9, y)
+    t.penup()
+    t.goto(x + 9, y - 400)
+    t.pendown()
+    t.goto(x + 9, y)
+    t.penup()
+
+
 def draw_flower(size, x, y, color, color_center, angle, n):
     print(str("Drawing " + str(n) + "-petal " + color + " flower with " + color_center + " center at position x:" + str(x) + " and y:" + str(y) + "."))
-    t.goto(x, y)
+
+    draw_stick(x, y)
 
     for j in range(n):
         t.setheading(angle + (360 * j // n))
@@ -69,31 +90,47 @@ def draw_sun():
     t.end_fill()
 
 
-colors = ["magenta", "cyan", "navy", "blue", "red", "yellow", "orange", "green"]
+colors = ["magenta", "pink", "navy", "blue", "red", "yellow", "orange", "white"]
 
 # --------------------
 
+print()
+print("Started")
 t.bgcolor("cyan")
 t.hideturtle()
 t.penup()
 t.speed(0)
-
-# draw_sun()
-
-
-# draw_flower(50, 40, 40, "green", "yellow")
-# draw_flower(70, -140, -140, "black", "green")
-# draw_flower(50, 0, -100, "pink", "navy")
-# draw_flower(50, 80, 145, "navy", "cyan")
-# draw_flower(50, 100, -150, "orange", "yellow")
-i = 50
+t.pensize(3)
 
 draw_sun()
 
-for g in range(i):
-    i = i+1
-    draw_flower(randint(10, 40), randint(-300, 300), randint(-300, 50), choice(colors), choice(colors), randint(0, 90), randint(3, 6))
-    w.sleep(0.0001)
+# ---------------------------------------------------------------------
+# Examples:
+#   draw_flower(Size, X, Y, PetalColor, CenterColor, Angle, PetalCount)
+#
+#   randint(1, 6)
+#     Returned value may be: "4";
+#
+#   choise("Python", "HTML", "JavaScript")
+#     Returned value may be: "Python";
+#
+# ---------------------------------------------------------------------
+
+for i in range(4):
+    draw_flower(30, i * 100 + 50, i * 20, choice(colors), choice(colors), randint(-90, 180), randint(5, 7))
+    draw_flower(30, i * -100 - 50, i * 20, choice(colors), choice(colors), randint(-90, 180), randint(5, 6))
+
+for i in range(3):
+    draw_flower(35, i * 100 + 80, i * 20 - 120, choice(colors), choice(colors), randint(-90, 180), randint(5, 7))
+    draw_flower(35, i * -100 - 80, i * 20 - 120, choice(colors), choice(colors), randint(-90, 180), randint(5, 7))
+
+for i in range(3):
+    draw_flower(40, i * 100 + 70, i * 20 - 250, choice(colors), choice(colors), randint(-90, 180), randint(5, 7))
+    draw_flower(40, i * -100 - 70, i * 20 - 250, choice(colors), choice(colors), randint(-90, 180), randint(5, 7))
+
+draw_stick(0, -50)
+draw_flower(40, 0, -50, "navy", "cyan", 0, 7)
 
 print("Finished")
-t.exitonclick()
+
+t.mainloop()
